d7f98511b90175680220361bd5e94258ecf06be6
Chengxin Zhao <zchenxing2002@gmail.com>
2024-10-10 14:43:52 +0100

    change to sweep

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,11 @@
 #include <Arduino.h>
 
 Servo base_servo;
+Servo arm_servo;
 
 int base_pot_pin = 0;
 int arm_pot_pin = 1;
+int head_pot_pin = 2;
 
 int base_val;
 int arm_val;
@@ -21,6 +23,7 @@ int smoothAnalogRead(int pin) {
 
 void setup() {
     base_servo.attach(9);
+    arm_servo.attach(10);
     Serial.begin(9600);
 }
 
@@ -32,6 +35,7 @@ void loop() {
     arm_val = map(arm_val, 0, 1023, 0, 180);
 
     base_servo.write(base_val);
+    arm_servo.write(arm_val);
 
     Serial.print("Base Servo Value: ");
     Serial.println(base_val);
